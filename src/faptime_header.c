7f7e2d6d075f297720576f14ca8e10c8e4fbb600
Kyle Wanamaker <kyle@klowell.com>
2013-04-28 03:09:24 -0400

    Remove some bad math with headers. Change to fcgi status header

--- src/faptime_header.c
+++ src/faptime_header.c
@@ -7,40 +7,120 @@
 
 #include "faptime_header.h"
 
-#define HEADER_BUFFER_LENGTH 5000
+#define H_BUF_LEN 5000
 
-char *h_buf;
-int request_id;
+static char *h_buf = NULL;
+static int req_status = 0;
 
-int faptime_header_init() {
+
+int faptime_header_init()
+{
+	req_status = 200;
 	faptime_header_free();
-	if (NULL == (h_buf = malloc(HEADER_BUFFER_LENGTH+1))) {
+	if (NULL == (h_buf = malloc(sizeof(char) * H_BUF_LEN+1))) {
 		error_log("Failed to allocate header buffer");
 		return 0;
 	}
-	strncpy(h_buf, '\0', sizeof(*h_buf));
 	return 1;
 }
 
-int faptime_header_free() {
+int faptime_set_status(int status)
+{
+	int tmp = req_status;
+	req_status = status;
+	FCGX_SetExitStatus(status,
+			   faptime_get_stream(FAPTIME_STREAM_OUT));
+	debug_log("Setting req status to %d", status);
+	return tmp;
+}
+
+
+char *faptime_status_header(int code)
+{
+	char status[128] = { '\0' };
+	switch (code) {
+	case 200:
+		strcpy(status, "200 OK");
+		break;
+	case 201:
+		strcpy(status, "201 Created");
+		break;
+	case 301:
+		strcpy(status, "301 Moved Permanently");
+		break;
+	case 302:
+		strcpy(status, "302 Found");
+		break;
+	case 400:
+		strcpy(status, "400 Bad Request");
+		break;
+	case 404:
+		strcpy(status, "404 Not Found");
+		break;
+	case 405:
+		strcpy(status, "405 Method Not Allowed");
+		break;
+	case 422:
+		strcpy(status, "422 Unprocessable Entity");
+		break;
+	case 429:
+		strcpy(status, "429 Too Many Requests");
+		break;
+	case 500:
+	default:
+		strcpy(status, "500 Internal Server Error");
+		break;
+	}
+
+	char *header;
+	/* WARNING: This differs from CGI */
+	if (asprintf(&header, "Status: %s\r\n"
+		     "Content-Type: text/html\r\n", status) < 0 ) {
+		return NULL;
+	}
+	return header;
+}
+
+int faptime_header_print()
+{
+	int c = 0;
+	FCGX_Stream *out = faptime_get_stream(FAPTIME_STREAM_OUT);
+	/* If this is NULL what do we do? It's our status! */
+	char *status = faptime_status_header(req_status);
+	if (status == NULL) {
+		error_log("Unable to allocate memory for status header. "
+			  "This is probably fatal");
+		exit(1);
+	}
+
+	c += FCGX_FPrintF(out, status);
+	c += FCGX_FPrintF(out, h_buf);
+	c += FCGX_FPrintF(out, "\r\n");
+	debug_log("Trying to print headers:\n**%s%s\r\n", status, h_buf);
+	free(status);
+	return c;
+}
+
+int faptime_header_free()
+{
 	free(h_buf);
 	return 1;
 }
 
-int faptime_header_add(int rid, char *header) {
-	if (rid != request_id) {
-		if (!faptime_header_init())
-			return 0;
-		request_id = rid;
-	}
-
+int faptime_header_add(char *header)
+{
 	return (faptime_header_cat(header) >= 0);
 }
 
 int faptime_header_cat(char *header) {
 	/* @TODO: Check to see if header was \r\n terminated */
+	if (h_buf == NULL && !faptime_header_init()) {
+		error_log("Unexpected! h_buf was NULL");
+		exit(1);
+	}
+
 	int b_size = sizeof(h_buf);
-	if (strlen(header)+strlen("\r\n")+1 > b_size - strlen(h_buf)) {
+	if (strlen(header)+strlen("\r\n")+1 > H_BUF_LEN - strlen(h_buf)) {
 		error_log("Header buffer exhausted.\n");
 		return -1;
 	}
@@ -48,4 +128,16 @@ int faptime_header_cat(char *header) {
 	strcat(h_buf, header);
 	strcat(h_buf, "\r\n");
 	return b_size - strlen(h_buf);
+}
+
+int faptime_set_redirect(char *url)
+{
+	char *s = NULL;
+	if (asprintf(&s, "Location: %s", url) < 0 || s == NULL) {
+		error_log("Unable to allocate memory for redirect");
+		return 0;
+	}
+	faptime_header_add(s);
+	free(s);
+	return 1;
 }
--- src/faptime_header.h
+++ src/faptime_header.h
@@ -12,8 +12,14 @@
 #define HAVE_FAPTIME_HEADER
 
 int faptime_header_init();
-int faptime_header_add(int request_id, char *header);
+int faptime_header_add(char *header);
 int faptime_header_cat(char *header);
 int faptime_header_free();
+
+int faptime_header_print();
+
+int faptime_set_redirect(char *url);
+int faptime_set_status(int status);
+char *faptime_status_header(int code);
 
 #endif
